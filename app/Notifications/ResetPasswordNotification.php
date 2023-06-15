82c193d79a2e5aee6ed0ae540a2eb0244fd2fdfe
Dias Mukhametrakhim <190103159@stu.sdu.edu.kz>
2023-06-15 22:26:07 +0600

    update resetpassword

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -47,9 +47,11 @@ class ResetPasswordNotification extends Notification
      */
     public function toMail($notifiable)
     {
+        $url = url("/api/reset-password?token=" . $this->token);
+
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
-            ->action('Reset Password', url(config('app.url').route('password.reset', $this->token, false)))
+            ->action('Reset Password', $url)
             ->line('If you did not request a password reset, no further action is required.');
     }
 }
