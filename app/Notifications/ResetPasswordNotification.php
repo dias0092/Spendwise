34ce557d88849250db3f1b7d0d11909e203713df
Dias Mukhametrakhim <190103159@stu.sdu.edu.kz>
2023-06-20 23:09:30 +0600

    make correct changes

--- app/Notifications/ResetPasswordNotification.php
+++ app/Notifications/ResetPasswordNotification.php
@@ -47,7 +47,7 @@ class ResetPasswordNotification extends Notification
      */
     public function toMail($notifiable)
     {
-        $url = url("'https://spendwise-niyaztaye-gmailcom.vercel.app/restore-password/'?token=" . $this->token);
+        $url = url("https://spendwise-niyaztaye-gmailcom.vercel.app/restore-password?token=" . $this->token);
 
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
