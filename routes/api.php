3cce7525a8ef8e23acc4e1e055a8d00c16a2bf48
Dias Mukhametrakhim <190103159@stu.sdu.edu.kz>
2023-06-20 20:27:08 +0600

    delete them api

--- routes/api.php
+++ routes/api.php
@@ -24,7 +24,6 @@ Route::middleware(['already_authenticated'])->group(function () {
 Route::middleware(['auth:sanctum'])->group(function () {
     //User routes
     Route::apiResource('users', UserController::class);
-    Route::put('/users/{user}/theme', [UserController::class, 'updateTheme']);
     Route::put('users/settings', [UserController::class, 'updateSettings']);
     // Account routes
     Route::apiResource('accounts', AccountController::class);
